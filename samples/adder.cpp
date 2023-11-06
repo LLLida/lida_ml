04fbf111a63322a863a0017a0ae8613b7b8a540e
LLLida <0adilmohammad0@gmail.com>
2023-11-06 17:12:05 +0300

    seems to work
    I'm so stupid... I passed lr=0.1.
    
    Now everything seems to work leaving myself even more confused

--- samples/adder.cpp
+++ samples/adder.cpp
@@ -10,33 +10,41 @@ int main()
       .log     = log_func
     });
 
-  std::vector<float> inputs = {
-    0.2, 0.4,
-    0.6, 0.7,
-    2.8, 0.9,
-    0.1, -1.2,
-    0.9, 0.3
-  };
-  std::vector<float> targets(inputs.size()/2);
-  for (size_t i = 0; i < targets.size(); i++) {
-    targets[i] = inputs[2*i] + inputs[2*i+1];
+  // lida::rand_seed(time(NULL));
+
+  size_t count = 15;
+  uint32_t input_size = 2;
+  uint32_t output_size = 2;
+
+  std::vector<float> inputs(count*input_size);
+  std::vector<float> targets(count*output_size);
+  for (size_t i = 0; i < count; i++) {
+    float x = lida::rand_uniform()*5.0 - 2.5;
+    float y = lida::rand_uniform()*5.0 - 2.5;
+
+    inputs[input_size*i]   = x;
+    inputs[input_size*i+1] = y;
+
+    targets[output_size*i]   = x + 0.45*y;
+    targets[output_size*i+1] = x - y;
   }
 
-  uint32_t input_shape[] = {2, (uint32_t)targets.size()};
+  uint32_t input_shape[] = {input_size, (uint32_t)count};
   lida::Tensor input(std::span{inputs}, input_shape);
-  uint32_t target_shape[] = {1, (uint32_t)targets.size()};
+  uint32_t target_shape[] = {output_size, (uint32_t)count};
   lida::Tensor target(std::span{targets}, target_shape);
 
-  uint32_t w_shape[] = { 2, 1 };
+  uint32_t w_shape[] = { input_size, output_size };
   lida::Tensor w(w_shape, LIDA_FORMAT_F32);
+  w.fill_normal();
 
   lida::Compute_Graph cg{};
   cg.add_input("x", input_shape)
     .add_parameter(w)
     .add_gate(lida::mm());
 
-  lida::SGD_Optimizer optim(0.1);
-  for (int i = 0; i < 11; i++) {
+  lida::SGD_Optimizer optim(0.01);
+  for (int i = 0; i < 15; i++) {
     cg.set_input("x", input).forward();
     auto y = cg.get_output(0);
 
@@ -47,11 +55,11 @@ int main()
     cg.zero_grad()
       .backward(loss)
       .optimizer_step(optim);
-  }
 
-  uint32_t indices[] = {0, 0};
-  float* v = (float*)w.get(indices);
-  printf("Learned: %f %f\n", v[0], v[1]);
+    uint32_t indices[] = {0, 0};
+    float* v = (float*)w.get(indices);
+    printf("Learned: %f %f\n", v[0], v[1]);
+  }
 
   return 0;
 }
